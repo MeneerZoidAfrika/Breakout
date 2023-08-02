0571fd64080065d2dd4f3b80eb8a698d32ab1c7a
Dian <dian@gho.co.za>
2023-08-02 14:11:27 +0200

    Added moving functionality to the Paddle to move in the x-axis

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import random
 ################################ FUNCTIONS ################################
 
 def center_window(window, width, height):
+    """Function to center the main window"""
     window.update_idletasks()
 
     screen_width = window.winfo_screenwidth()
@@ -16,6 +17,17 @@ def center_window(window, width, height):
     y = (screen_height - height) // 2
 
     window.geometry(f"{width}x{height}+{x}+{y}")
+
+
+def move_paddle(event):
+    """Moves the paddle in the x-axis relative to where the mouse is pointing"""
+
+    canvas.coords(  # Changing the Position of the paddle
+        paddle,
+        event.x - PADDLE_WIDTH / 2, 660 + PADDLE_HEIGHT / 2,
+        event.x + PADDLE_WIDTH / 2, 660 - PADDLE_HEIGHT / 2
+    )
+    print(event.x, event.y)
 
 
 
@@ -40,6 +52,14 @@ COLOR_LEVEL6 = "#FFCB47"  # Lemon Yellow
 COLOR_LEVEL7 = "#5F9EA0"  # Cadet Blue
 COLOR_LEVEL8 = "#E56B6F"  # Coral Red
 
+# BRICK SETTINGS
+BRICK_WIDTH = 100
+BRICK_HEIGHT = 50
+
+# PADDLE_SETTINGS
+PADDLE_WIDTH = 80
+PADDLE_HEIGHT = 20
+
 
 # Initializing GUI
 root = tk.Tk()
@@ -47,7 +67,7 @@ root.title("Breakout Modernized")
 
 # Setting the Width + Height of the Window
 window_height = 800
-window_width = 1210
+window_width = 1200
 root.geometry(f"{window_width}x{window_height}")
 
 # Setting the background of the Window
@@ -59,6 +79,7 @@ root.iconbitmap("images/favicon.ico")
 # Creating a canvas
 canvas = tk.Canvas(root, width=window_width+50, height=window_height+50, bg=COLOR1, highlightthickness=0,
                    borderwidth=0, highlightbackground=COLOR1)
+canvas.bind("<Motion>", move_paddle)  # Allowing the canvas to get the position of the mouse
 canvas.pack()
 
 # Making a 12x12 grid of Rectangles with different colors
@@ -66,14 +87,15 @@ x_increase = 0
 y_increase = 0
 rect_colors = [COLOR_LEVEL1, COLOR_LEVEL2, COLOR_LEVEL3, COLOR_LEVEL4, COLOR_LEVEL5, COLOR_LEVEL6, COLOR_LEVEL7, COLOR_LEVEL8]
 
+
 for i in range(12):
-    x_increase = i * 100  # Separate variable for x_increase
+    x_increase = i * 99  # Separate variable for x_increase
     for j in range(8):
         y_increase = j * 35
-        canvas.create_rectangle(10 + x_increase, 25 + y_increase, 100 + x_increase, 50 + y_increase, fill=rect_colors[::-1][j])
+        canvas.create_rectangle(10 + x_increase, 25 + y_increase, BRICK_WIDTH + x_increase, BRICK_HEIGHT + y_increase, fill=rect_colors[::-1][j])
 
 # Creating the paddle
-paddle = canvas.create_rectangle(100, 200, 400, 300, fill="white")
+paddle = canvas.create_rectangle(450, 650, 750, 700, fill="white")
 
 if __name__ == '__main__':
     center_window(root, window_width, window_height)
